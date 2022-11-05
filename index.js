9d1514bb7c70c6de5f6cfad2b99cc2f49d2a98ad
Camila Conde <camila.conde.lorente@gmail.com>
2022-11-04 23:58:13 -0400

    finished evalue rank function

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const evalueRank = (array) => {
-	return [1];
+	let rank = [1];
+
+	for (let i = 0; i < array.length; i++) {
+		if (rank.length == array.length) {
+			break;
+		}
+
+		if (array[i] != array[i + 1]) {
+			rank.push(rank[i] + 1);
+		}
+		else {
+			rank.push(rank[i]);
+		}
+	}
+
+	return rank;
 }
 
 module.exports = { evalueRank };
--- index.spec.js
+++ index.spec.js
@@ -1,9 +1,134 @@
 const { evalueRank } = require('./index');
 
-describe('tests the evaluation of rank', () => {
+describe('tests the rank array', () => {
 
 	it("returns list of ranks", () => {
-		const res = evalueRank([1]);
+		const res = evalueRank([100]);
 		expect(res).toEqual([1]);
+	});
+
+	it("returns list of ranks of 2 different numbers array", () => {
+		const res = evalueRank([100, 90]);
+		expect(res).toEqual([1, 2]);
+	});
+
+	it("returns list of ranks of 2 equal numbers array", () => {
+		const res = evalueRank([100, 100]);
+		expect(res).toEqual([1, 1]);
+	});
+
+	it("returns list of ranks of 3 different numbers array", () => {
+		const res = evalueRank([100, 90, 80]);
+		expect(res).toEqual([1, 2, 3]);
+	});
+
+	it("returns list of ranks of 3 different numbers array", () => {
+		const res = evalueRank([100, 90, 90]);
+		expect(res).toEqual([1, 2, 2]);
+	});
+
+	it("returns list of ranks of 3 different numbers array", () => {
+		const res = evalueRank([100, 100, 80]);
+		expect(res).toEqual([1, 1, 2]);
+	});
+
+	it("returns list of ranks of 3 different numbers array", () => {
+		const res = evalueRank([100, 100, 100]);
+		expect(res).toEqual([1, 1, 1]);
+	});
+
+	it("returns list of ranks of 4 different numbers array", () => {
+		const res = evalueRank([100, 90, 80, 70]);
+		expect(res).toEqual([1, 2, 3, 4]);
+	});
+
+	it("returns list of ranks of 4 different numbers array", () => {
+		const res = evalueRank([100, 90, 80, 80]);
+		expect(res).toEqual([1, 2, 3, 3]);
+	});
+
+	it("returns list of ranks of 4 different numbers array", () => {
+		const res = evalueRank([100, 90, 90, 70]);
+		expect(res).toEqual([1, 2, 2, 3]);
+	});
+
+	it("returns list of ranks of 4 different numbers array", () => {
+		const res = evalueRank([100, 90, 90, 90]);
+		expect(res).toEqual([1, 2, 2, 2]);
+	});
+
+	it("returns list of ranks of 4 different numbers array", () => {
+		const res = evalueRank([100, 100, 80, 80]);
+		expect(res).toEqual([1, 1, 2, 2]);
+	});
+
+	it("returns list of ranks of 4 different numbers array", () => {
+		const res = evalueRank([100, 100, 100, 70]);
+		expect(res).toEqual([1, 1, 1, 2]);
+	});
+
+	it("returns list of ranks of 4 different numbers array", () => {
+		const res = evalueRank([100, 100, 100, 100]);
+		expect(res).toEqual([1, 1, 1, 1]);
+	});
+
+	it("returns list of ranks of 5 different numbers array", () => {
+		const res = evalueRank([100, 100, 100, 100, 100]);
+		expect(res).toEqual([1, 1, 1, 1, 1]);
+	});
+
+	it("returns list of ranks of 5 different numbers array", () => {
+		const res = evalueRank([100, 100, 100, 100, 90]);
+		expect(res).toEqual([1, 1, 1, 1, 2]);
+	});
+
+	it("returns list of ranks of 5 different numbers array", () => {
+		const res = evalueRank([100, 100, 100, 90, 90]);
+		expect(res).toEqual([1, 1, 1, 2, 2]);
+	});
+
+	it("returns list of ranks of 5 different numbers array", () => {
+		const res = evalueRank([100, 100, 90, 90, 90]);
+		expect(res).toEqual([1, 1, 2, 2, 2]);
+	});
+
+	it("returns list of ranks of 5 different numbers array", () => {
+		const res = evalueRank([100, 90, 90, 90, 90]);
+		expect(res).toEqual([1, 2, 2, 2, 2]);
+	});
+
+	it("returns list of ranks of 5 different numbers array", () => {
+		const res = evalueRank([100, 90, 90, 90, 80]);
+		expect(res).toEqual([1, 2, 2, 2, 3]);
+	});
+
+	it("returns list of ranks of 5 different numbers array", () => {
+		const res = evalueRank([100, 90, 90, 80, 80]);
+		expect(res).toEqual([1, 2, 2, 3, 3]);
+	});
+
+	it("returns list of ranks of 5 different numbers array", () => {
+		const res = evalueRank([100, 90, 80, 80, 80]);
+		expect(res).toEqual([1, 2, 3, 3, 3]);
+	});
+
+	it("returns list of ranks of 5 different numbers array", () => {
+		const res = evalueRank([100, 90, 80, 80, 80]);
+		expect(res).toEqual([1, 2, 3, 3, 3]);
+	});
+
+	it("returns list of ranks of 5 different numbers array", () => {
+		const res = evalueRank([100, 90, 80, 80, 70]);
+		expect(res).toEqual([1, 2, 3, 3, 4]);
+	});
+
+	it("returns list of ranks of 5 different numbers array", () => {
+		const res = evalueRank([100, 90, 80, 70, 70]);
+		expect(res).toEqual([1, 2, 3, 4, 4]);
+	});
+
+	it("returns list of ranks of 5 different numbers array", () => {
+		const res = evalueRank([100, 90, 80, 70, 60]);
+		expect(res).toEqual([1, 2, 3, 4, 5]);
 	});
 });
